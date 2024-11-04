60ba6347edf0fc0cfa75f91ac130407a1b08a5d2
Felipe Pereira <felipepereirabritto08@gmail.com>
2024-11-04 01:20:37 -0300

    bugfix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_session import Session
+from combustiveis.gasolina_adtivada import GasolinaAditivada
 import pyqrcode
 import pickle
 import os
@@ -7,6 +8,7 @@ import os
 app = Flask(__name__)
 app.secret_key = 'Combustex_posto'
 app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_COOKIE_NAME'] = 'session'
 Session(app)
 
 USUARIOS_FILE = 'usuarios.pkl'
@@ -22,25 +24,6 @@ def save_usuarios(usuarios):
         pickle.dump(usuarios, file)
 
 usuarios = load_usuarios()
-
-class GasolinaAditivada:
-    def __init__(self):
-        self.preco_por_litro = 5.80
-        self.quantidade_disponivel = 3000
-
-    def abastecer_por_litros(self, litros):
-        if litros > self.quantidade_disponivel:
-            return "Quantidade de gasolina aditivada indisponível para abastecimento."
-        custo = litros * self.preco_por_litro
-        self.quantidade_disponivel -= litros
-        return f"Você abasteceu {litros:.2f} litros de gasolina aditivada, que custaram R$ {custo:.2f}."
-
-    def abastecer_por_valor(self, valor):
-        litros = valor / self.preco_por_litro
-        if litros > self.quantidade_disponivel:
-            return "Quantidade de gasolina aditivada indisponível para abastecimento."
-        self.quantidade_disponivel -= litros
-        return f"Você abasteceu R$ {valor:.2f} de gasolina aditivada, que equivale a {litros:.2f} litros."
 
 gasolina_aditivada = GasolinaAditivada()
 
